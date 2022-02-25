8fcd9e85f7251923ca49a84099c850191ad9d4f1
earthpulse <it@earthpulse.es>
2022-02-25 18:13:02 +0100

    update

--- pytorch_eo/config/config.py
+++ pytorch_eo/config/config.py
@@ -1,2 +1,6 @@
-SCAN_URL = 'http://192.168.0.14:8003'
-SPAI_URL = 'http://192.168.0.14:8000'
+# SCAN_URL = 'http://192.168.0.14:8003'
+# SPAI_URL = 'http://192.168.0.14:8000'
+
+
+SCAN_URL = 'https://scan.api.earthpulse.ai'
+SPAI_URL = 'https://spai-mvp.earthpulse.ai'
